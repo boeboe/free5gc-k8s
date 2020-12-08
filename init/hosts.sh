3bf19e620aa09b001dcf4738795d5a97fda9ac38
Bart Van Bos <bartvanbos@gmail.com>
2020-12-08 01:15:56 +0100

    added dnsmasq for local dns resolving

--- init/hosts.sh
+++ init/hosts.sh
@@ -9,7 +9,6 @@ sudo tee -a /etc/hosts << END
 10.1.1.7    node2
 10.1.1.8    node3
 10.1.1.9    node4
-
 END
 
 sudo tee -a /etc/hosts << END
@@ -17,5 +16,19 @@ sudo tee -a /etc/hosts << END
 # Host aliases for extra services
 10.1.1.4    elasticsearch
 10.1.1.4    kibana
+END
 
+sudo tee -a /etc/hosts << END
+
+# Host aliases for istio gateway exposed workloads
+10.1.1.5    f5gc-webui
+10.1.1.6    f5gc-webui
+10.1.1.7    f5gc-webui
+10.1.1.8    f5gc-webui
+10.1.1.9    f5gc-webui
+10.1.1.5    mongo-express
+10.1.1.6    mongo-express
+10.1.1.7    mongo-express
+10.1.1.8    mongo-express
+10.1.1.9    mongo-express
 END
